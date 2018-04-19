a974f7e5498631eb153cc0d4a154343fdcf5431e
emmassr <31829603+emmassr@users.noreply.github.com>
2018-04-19 16:02:57 +0100

    commit

---
+++ ex5_2.rb
@@ -0,0 +1,11 @@
+print "What is the first number?"
+first_number = gets.chomp.to_i
+print "What is the second number?"
+second_number = gets.chomp.to_i
+
+puts "#{first_number} + #{second_number} = #{first_number + second_number}"
+puts "#{first_number} - #{second_number} = #{first_number - second_number}"
+puts "#{first_number} * #{second_number} = #{first_number * second_number}"
+puts "#{first_number} / #{second_number} = #{first_number / second_number}"
+
+	

957de33cb2d256ae3ab84711d5a0db31a8c54e4c
emmassr <31829603+emmassr@users.noreply.github.com>
2018-04-19 15:03:43 +0100

    first commit

---
+++ ex3_2.rb
@@ -0,0 +1,7 @@
+print "What is the quote?"
+quote = "These aren't the droids you are looking for."
+puts "#{quote}"
+print "Who said it?"
+author = "Obi-Wan Kenobi"
+puts "#{author}"
+print "Obi-Wan Kenobi says, \"#{quote}\" "  # this return a % at the end of the sentence for some reason

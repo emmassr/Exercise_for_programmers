f979e1d6fb0a63ef9fd51a410b88f6f243c14fe2
emmassr <31829603+emmassr@users.noreply.github.com>
2018-04-19 15:39:13 +0100

    commit

---
+++ ex4_2.rb
@@ -0,0 +1,9 @@
+print "Enter a noun:"
+noun = gets.chomp
+print "Enter a verb:"
+verb = gets.chomp
+print "Enter an adjective:"
+adjective = gets.chomp
+print "Enter an adverb:"
+adverb = gets.chomp
+puts "Do you walk your #{adjective} #{noun} #{adverb}? That is hilarious!"
---
+++ test.rb
@@ -0,0 +1,18 @@
+print "What is the bill?"
+bill_amount = gets
+puts "The bill is £#{bill_amount}"
+print"What is the tip percentage?"
+tip_percentage = gets
+puts "The tip percentage is #{tip_percentage}"
+print "What is the tip?"
+tip = gets
+ if tip < 0
+	puts "Please enter a positive number"
+	else
+	puts "Test"
+end
+
+
+
+
+
